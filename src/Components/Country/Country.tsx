7226a73c14d4b6e4017662232dc51ae56e0d46da
Sajib Hosen <sajib.201h@gmail.com>
2022-03-22 08:26:28 +0600

    done but style

--- src/Components/Country/Country.tsx
+++ src/Components/Country/Country.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { countryDataType, weatherDataType } from '../Types/countryData';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Box, Button } from '@mui/material';
 
 type countryProps = {
@@ -11,6 +11,11 @@ type countryProps = {
 const Country = (props: countryProps ) => {
     const { country, weather } = props;
     const navigate = useNavigate()
+    const [ weatherShow, setWeatherShow ] = useState(false)
+
+    const showWeather = ( shouldShow: boolean )=>{
+        setWeatherShow(shouldShow)
+    }
 
     console.log(country, weather)
 
@@ -27,9 +32,19 @@ const Country = (props: countryProps ) => {
             <p>population: { country?.population}</p>
             <img src={country?.flags.png} alt="flag" />
             <p>latitude: {country?.latlng[0]} and longitude: {country?.latlng[1]} </p>
+            { weatherShow ?             <div>
+                <img src={weather?.weatherIcon} alt="weather icon" />
+                <p>Temperature: {weather?.temperature}</p>
+                <p>Wind Speed: {weather?.windSpeed}</p>
+                <p>Precip: {weather?.precip}</p>
+            </div> : 
             <Box sx={{ display: 'flex', justifyContent: 'center', p: 1, m: 1, bgcolor: 'background.paper', borderRadius: 1, height:'100%' }} >
-               <Button disabled={false} variant="contained">Capital weather</Button>
-            </Box>
+                <Button onClick={()=>{showWeather(true)}} disabled={false} variant="contained">Capital weather</Button>
+            </Box> }
+
+            <Button >
+                <Link to='/'  >Back Home</Link>
+            </Button>
         </div>
     );
 };
